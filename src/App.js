bd50c59b52d77b2f762d349cccbc55f383630759
Connor McGinn <connor.mcginn@daugherty.com>
2020-02-12 13:54:21 -0600

    initial commit.. headers only

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
+import Heading from './components/Heading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      people: [],
+    };
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <Title title="Star Wars" />
+        <Heading headingText="People" />
+      </div>
+    );
+  };
+};
 
 export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Heading = props => {
+    return <h3>{props.headingText}</h3>;
+};
+
+export default Heading;
